bb1a84d92f80cd213690377aec76b19bd44164d2
tom <tomhall4321@gmail.com>
2020-11-10 11:46:53 +0000

    adding address field columns to migration file

---
+++ database/migrations/2020_11_10_113928_add_address_fields_to_owners_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAddressFieldsToOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('owners', function (Blueprint $table) {
+            $table->string("address_1", 100);
+            $table->string("address_2", 100)->nullable();
+            $table->string("town", 50);
+            $table->string("postcode", 9);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('owners', function (Blueprint $table) {
+            $table->dropColumn("address_1");
+            $table->dropColumn("address_2");
+            $table->dropColumn("town");
+            $table->dropColumn("postcode");
+        });
+    }
+}
